d31f7a404a99d323c5b455d4b02e972711c0fc4b
Osko <oskodev@gmail.com>
2026-08-06 22:04:06 -0600

    update 1.6.3

--- syst/SysExcp.py
+++ syst/SysExcp.py
@@ -10,6 +10,7 @@ class Stage:
    PERMISSIONS = 'permissions'
    MRSETPERMS = '[m_r] set_permissions'
    HMRSETPERMS = '[hm_r] set_permissions'
+
 #
 
 async def _forbidden_(interaction, context, exc, send):
